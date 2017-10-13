83c39a29a10e54c119f04e4e1fa9a6858811be46
Al Mamun <asifulmamun@users.noreply.github.com>
2017-10-13 22:21:18 +0600

    Create theme-development-some-code.php
    Code for theme development.

---
+++ theme-development-some-code.php
@@ -0,0 +1,2 @@
+<?php bloginfo('template_directory'); ?> //For Linkup JavaScript. Before Directory Location.
+<?php bloginfo('stylesheet_directory'); ?> //For Linkup Image & Css. Before Directory Location.
