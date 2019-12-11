481aaa45bd2323fedf518c48925ad353475e3da2
Al Mamun <asifulmamun@users.noreply.github.com>
2019-12-11 20:54:06 +0600

    Make like as plugin

--- wp-custom-login-logo.php
+++ wp-custom-login-logo.php
@@ -1,4 +1,12 @@
-/* Paste any wher in themes functios.php */
+<?php
+/**
+* Plugin Name: Your Site Name
+* Plugin URI: https://github.com/asifulmamun
+* Description: This plugin requried this site. It's using custom functions.
+* Version: 1.0
+* Author: Al Mamun
+* Author URI: http://facebook.com/asifulmamun
+**/
 
 /* Custom login page logo */
 add_action('login_head','custom_login_logo');
@@ -6,3 +14,8 @@ function custom_login_logo() {
 	echo '<style type="text/css">h1 a { background-image: url(http://login.png) !important; background-size: 80% auto !important;
 width: 90% !important; height: 75px !important; }</style>'; 
 }
+
+
+
+// --- Plugin End
+?>
